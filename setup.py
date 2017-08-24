5ec2ba20af99ddd34f61af496020fbd6752c0daa
Constant Bridon <constant.bridon@gmail.com>
2017-08-24 16:16:40 +0200

    commit with setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='feature_importance_gbm',
+      version='0.1',
+      description='Computation of feature importance for gradient boosting ensemble methods',
+      url='https://github.com/Cnstant/feature_importance_gbm',
+      author='Constant Bridon',
+      author_email='constant.bridon@example.com',
+      license='MIT',
+      packages=['feature_importance_gbm'],
+      zip_safe=False)
